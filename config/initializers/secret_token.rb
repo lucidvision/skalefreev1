470c9123d8628407c8ba0922b84db9b269a527aa
brian <brianpark25@gmail.com>
2013-07-23 12:42:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skalefreev1::Application.config.secret_token = '1a720d9e6995256948187a971f3edfe5a4137b7ddbef3bfa512e3250f50d9e1671cdab187ce6a4bd2a11c1910fb7170b43ed0ab9f755d82c5d8603aa9967b13e'
