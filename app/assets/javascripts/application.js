315d4abe6dc1ee22eea461c920b38b5b6658f556
brian <brianpark25@gmail.com>
2013-09-22 12:47:28 -0700

    vender compiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require twitter/bootstrap
 //= require jquery
+//= require twitter/bootstrap
+//= require jquery.mobile
 //= require jquery.turbolinks
 //= require jquery_ujs
