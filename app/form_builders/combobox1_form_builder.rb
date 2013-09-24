f75603ccfcd43fcaf97de3e33afd7a1bf76412da
brian <brianpark25@gmail.com>
2013-09-24 12:05:13 -0700

    combofix

--- app/form_builders/combobox1_form_builder.rb
+++ app/form_builders/combobox1_form_builder.rb
@@ -1,9 +1,9 @@
-class ComboboxFormBuilder < ActionView::Helpers::FormBuilder
+class Combobox1FormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Context # for nested content_tag
   include ActionView::Helpers::FormTagHelper #for sanitize_to_id method access
 
   def combobox_tag(name, options, opts= {})
-     @template.content_tag(:input, :name => "findpost["+sanitize_to_id(name)+"]", :id => "findpost_"+sanitize_to_id(name), :type => "text", :list => opts[:list_id]) do
+     @template.content_tag(:input, :name => "q["+sanitize_to_id(name)+"]", :id => "q_"+sanitize_to_id(name), :type => "text", :list => opts[:list_id]) do
        content_tag(:datalist, :id => opts[:list_id]) {options}
      end    
   end    
