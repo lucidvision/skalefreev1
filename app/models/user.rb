2badeb2e915bca267103d79ad149c06860b044ba
brian <brianpark25@gmail.com>
2013-10-08 01:35:25 -0700

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   acts_as_messageable
 
