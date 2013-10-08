d4ce5aa9d9d8a139e1c02807ce87b8639a13c581
brian <brianpark25@gmail.com>
2013-10-08 01:16:15 -0700

    return

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby '2.0.0'
 gem 'rails', '3.2.13'
 
 gem 'rake'
-gem 'devise'
+gem 'devise', '3.0.2'
 gem 'therubyracer'
 gem 'jquery-rails'
 gem 'jquery-ui-rails'
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -168,7 +168,7 @@ Devise.setup do |config|
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  # config.reset_password_keys = [ :email ]
+  config.reset_password_keys = [ :email ]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
@@ -188,8 +188,6 @@ Devise.setup do |config|
   # ==> Configuration for :token_authenticatable
   # Defines name of the authentication token params key
   # config.token_authentication_key = :auth_token
-  config.allow_insecure_token_lookup = true
-  config.allow_insecure_sign_in_after_confirmation = true
 
   # ==> Scopes configuration
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
