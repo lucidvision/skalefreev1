05d9ef6f883cbc5f4d672d5f5d4e56f733ae8427
brian <brianpark25@gmail.com>
2013-10-08 12:11:25 -0700

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(current_user)
     root_path
   end
 end
