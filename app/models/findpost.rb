5d8b4a886c3f0ff396026f9ae6c27bbd3b91da3e
brian <brianpark25@gmail.com>
2013-11-10 18:33:10 -0800

    fix

--- app/models/findpost.rb
+++ app/models/findpost.rb
@@ -4,7 +4,7 @@ class Findpost < ActiveRecord::Base
 
   validates :user_id, presence: true
   validates :c1, presence: true, length: { maximum: 75 }
-  validates :c2, length: { maximum: 75 }
+  validates :c2, presence: true, length: { maximum: 75 }
   validates :c3, length: { maximum: 75 }
   validates :c4, length: { maximum: 75 }
   validates :c5, length: { maximum: 75 }
