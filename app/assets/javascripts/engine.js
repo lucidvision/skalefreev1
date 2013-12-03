fb5edd86c3ce8ec12cbf4e0fbef8b4652623ff4d
brian <brianpark25@gmail.com>
2013-12-02 22:28:14 -0800

    rework

--- app/assets/javascripts/engine.js
+++ app/assets/javascripts/engine.js
@@ -11,6 +11,7 @@ function store_search()
 		alert("Please select a wall.");
 	}
 	$('#wall_selection').empty().append(wall.value);
+	$('#wall_heading').empty().append(wall.value);
 	$('#wallpost').val(wall.value);
 }
 
