09a429ba80dfe6548174ca45df53b45453e0c9f7
brian <brianpark25@gmail.com>
2014-01-21 01:26:27 -0800

    here

--- app/form_builders/combobox2_form_builder.rb
+++ app/form_builders/combobox2_form_builder.rb
@@ -1,9 +1,9 @@
-class Combobox1FormBuilder < ActionView::Helpers::FormBuilder
+class Combobox2FormBuilder < ActionView::Helpers::FormBuilder
   include ActionView::Context # for nested content_tag
   include ActionView::Helpers::FormTagHelper #for sanitize_to_id method access
 
   def combobox_tag(name, options, opts= {})
-     @template.content_tag(:input, :name => "q["+sanitize_to_id(name)+"]", :placeholder => "ENGL 100", :id => "wall", :type => "text", :list => opts[:list_id]) do
+     @template.content_tag(:input, :name => "q["+sanitize_to_id(name)+"]", :placeholder => "Double Click for Walls", :id => "wall", :type => "text", :list => opts[:list_id]) do
        content_tag(:datalist, :id => opts[:list_id]) {options}
      end    
   end    
